88caa7c730be2ce17a468121d5a45b7bb7755fa8
Ramon Hernandez <ray@starscream.gateway.2wire.net>
2009-11-16 19:14:15 -0500

    Added a small hack to colorize the nicknames.
    The hack can't possibly scale well.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,35 +23,4 @@ class ApplicationController < ActionController::Base
     session[:nickname] = fake_name if session[:nickname].nil?
   end
 
-  def set_color
-    colors = [
-      '#fec94f',
-      '#edd400',
-      '#c4a000',
-      '#fcaf3e',
-      '#f57900',
-      '#ce5c00',
-      '#e9b96e',
-      '#c17d11',
-      '#8f5902',
-      '#8ae234',
-      '#73d216',
-      '#4e9a06',
-      '#729fcf',
-      '#3465a4',
-      '#204a87',
-      '#ad7fa8',
-      '#75507b',
-      '#5c3566',
-      '#cf2929',
-      '#cc0000',
-      '#a40000',
-      '#eeeeec',
-      '#d3d7cf',
-      '#babdb6'
-    ]
-    
-    session[:color] = colors[rand(colors.length)] if session[:color].nil?
-  end
-
 end
--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,2 +1,32 @@
 module MessagesHelper
+  def get_existing_or_random_color(nick)
+    colors = [
+      '#fec94f',
+      '#edd400',
+      '#c4a000',
+      '#fcaf3e',
+      '#f57900',
+      '#ce5c00',
+      '#e9b96e',
+      '#c17d11',
+      '#8f5902',
+      '#8ae234',
+      '#73d216',
+      '#4e9a06',
+      '#729fcf',
+      '#3465a4',
+      '#204a87',
+      '#ad7fa8',
+      '#75507b',
+      '#5c3566',
+      '#cf2929',
+      '#cc0000',
+      '#a40000',
+      '#eeeeec',
+      '#d3d7cf',
+      '#babdb6'
+      ]
+
+      session[nick] = session[nick] || colors[rand(colors.length)]
+  end
 end
